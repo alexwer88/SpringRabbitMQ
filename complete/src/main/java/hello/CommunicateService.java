e25442281dcb4dba5467b659a717b91e2c0c9c5d
Верижников Алексей Станиславович <A.Verizhnikov@msr.lan>
2018-04-28 17:59:41 +0300

    first commit

--- complete/src/main/java/hello/CommunicateService.java
+++ complete/src/main/java/hello/CommunicateService.java
@@ -1,6 +1,9 @@
 package hello;
 
 import java.util.concurrent.CountDownLatch;
+
+import hello.config.RabbitConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,9 +11,10 @@ public class Receiver {
 
     private CountDownLatch latch = new CountDownLatch(1);
 
+    @RabbitListener(queues = RabbitConfig.queueName)
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        latch.countDown();
+      //  latch.countDown();
     }
 
     public CountDownLatch getLatch() {
